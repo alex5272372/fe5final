2b4f885e39f54b26ffe0df095258f214f1ea02c2
alex5272372 <alex5272372@gmail.com>
2019-07-26 22:01:28 +0300

    CW26072019

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './reducers/index';
+import rootReducer from './reducers';
 import logger from 'redux-logger';
 
 const store = createStore(rootReducer, applyMiddleware(logger));
