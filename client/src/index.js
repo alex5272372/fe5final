a29ce234d7fefa27ef9b8898bcdf648a954c0ad8
alex5272372 <alex5272372@gmail.com>
2019-08-04 18:16:40 +0300

    package.json edited 2

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 import './index.css';
@@ -16,8 +15,3 @@ render((
         </BrowserRouter>
     </Provider>
 ), document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
